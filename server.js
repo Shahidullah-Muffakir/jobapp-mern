1d676b9d543c9a48c41e18cf47348fbb12162bdf
“shahidullah <shahidmuffakir@gmail.com>
2023-01-23 17:37:20 +0530

    modfied server

--- server.js
+++ server.js
@@ -45,18 +45,14 @@ app.use(morgan("dev"));
 
 
 // ===================Routes=================================================================================================================
-app.get("/", (req, res) => {
-  res.json({ msg: "welcome" });
-});
-app.get("/api/v1", (req, res) => {
-  res.json({ msg: "welcome" });
-});
 
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
-app.get('*', function (request, response) {
-  response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+
+// only when ready to deploy
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
 
 // ===================Internal Middlewares=================================================================================================================
